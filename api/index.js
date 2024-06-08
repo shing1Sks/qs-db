fcb3ba2f48eae988364fdbff7324e11f126087f3
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-08 19:34:05 +0530

    try 1

--- api/index.js
+++ api/index.js
@@ -1,21 +1,11 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectToDB from "op-db/dbMongo.js";
 import expressApp from "op-db/expressApp.js";
 
 dotenv.config({path: "../.env"});
 
-const app = express();
 
 // Connect to the database
 connectToDB();
 
-// Use the expressApp middleware
-app.use(expressApp);
-
-// Route definitions
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-export default app;
+export default expressApp;
