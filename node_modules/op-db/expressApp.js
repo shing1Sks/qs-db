b64a8df9874456938080da5f2e28ecd29eba565d
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-08 21:34:44 +0530

    try again

--- node_modules/op-db/expressApp.js
+++ node_modules/op-db/expressApp.js
@@ -3,22 +3,35 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = e();
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}));
-app.use(e.json({limit:"32kb"}));
-app.use(e.urlencoded({extended:true,limit:"32kb"}));
+var corsOptions = {};
+if (process.env.CORS_ORIGIN == "*") {
+  corsOptions = {
+    origin: (origin, callback) => {
+      callback(null, origin);
+    },
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+} else {
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+}
+
+app.use(e.json({ limit: "32kb" }));
+app.use(e.urlencoded({ extended: true, limit: "32kb" }));
 app.use(e.static("public"));
-app.use(cookieParser());    
+app.use(cookieParser());
 
 //routes import section - 1
 import userRoutes from "./routes/user.routes.js";
 import postsRoutes from "./routes/posts.routes.js";
 
 //routes declaration section - 2
-app.use("/api/user",userRoutes);
-app.use("/api/posts",postsRoutes);
-
+app.use("/api/user", userRoutes);
+app.use("/api/posts", postsRoutes);
 
 export default app;
