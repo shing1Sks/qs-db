74664059075ed4e6c41dabb239427a0f214de18f
Shreyash Kumar Singh <141475077+shing1Sks@users.noreply.github.com>
2024-06-08 19:53:04 +0530

    try

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
 import dotenv from "dotenv";
-import connectToDB from "op-db/dbMongo.js";
-import expressApp from "op-db/expressApp.js";
-
+import main from "op-db";
 dotenv.config({path: "../.env"});
 
 
-// Connect to the database
-connectToDB();
-const app = expressApp();
-export default app;
+main();
